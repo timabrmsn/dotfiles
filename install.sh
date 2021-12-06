b9b87c304ec3d356aab3e7fec9a271cbf62ecec5
tim.abramson <tim.abramson@code42.com>
2021-12-06 08:44:24 -0600

    add zsh config

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
+#!/bin/zsh
+
+# tmux
 cp tmux.conf ~/.tmux.conf
+
+# neovim
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/
 nvim +PlugInstall +qall
+
+# zsh
+
+if [ ! -d ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+cp auto_env.sh ~/.oh-my-zsh
+cp zshrc ~/.zshrc
